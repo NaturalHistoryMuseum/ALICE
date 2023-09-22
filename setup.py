431c64528d1b543ee0bd57161fa4347116d3ca2f
Ben Scott <ben@benscott.co.uk>
2023-09-22 17:24:49 +0100

    switch setup to pyproject

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="alice",
-    version="2.0",
-    description='ALICE: Angled Label Somthing Capture Equipment',
-    url='http://github.com/naturalhistorymuseum/ALICE',
-    author='',
-    author_email='',    
-    packages=["alice", "alice/tasks"],
-    # package_data={'alice': ['alice/**/*']},
-    # packages=find_packages(),
-    # packages=find_packages(include=('alice',)),
-    include_package_data=True,
-    install_requires=[
-        'torch==2.0.1'  # torch is required before installing detectron
-    ],
-)
+setup()
+
+
+# from setuptools import find_packages, setup
+
+# setup(
+#     name="alice",
+#     version="2.0",
+#     description='ALICE: Angled Label Somthing Capture Equipment',
+#     url='http://github.com/naturalhistorymuseum/ALICE',
+#     author='',
+#     author_email='',    
+#     packages=["alice", "alice/tasks"],
+#     # package_data={'alice': ['alice/**/*']},
+#     # packages=find_packages(),
+#     # packages=find_packages(include=('alice',)),
+#     include_package_data=True,
+#     install_requires=[
+
+#     ],
+#     entry_points={
+#         'console_scripts': [
+#             'run = scripts.run:main',
+#         ],
+#     },    
+# )
 
