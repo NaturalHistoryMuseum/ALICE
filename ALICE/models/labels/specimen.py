73ea2879f06e300e35340bdfe9a14dd587f8f97f
Ben Scott <ben@benscott.co.uk>
2023-09-24 20:01:13 +0100

    Add CLIP clustering

--- ALICE/models/labels/specimen.py
+++ ALICE/models/labels/specimen.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 from typing import List
-from scipy.stats import mode
 import re
+import itertools
+from collections import defaultdict
+import imutils
+from PIL import Image
+import numpy as np
 
-from alice.models.labels import AngledView, LabelValid, InvalidLabel, LabelQuartet
-from alice.models.text.alignment import TextAlignment
-from alice.config import PROCESSING_IMAGE_DIR, logger, OUTPUT_DATA_DIR
+
+from alice.models.labels import AngledView, InvalidLabel, LabelQuartet
+from alice.config import RESIZED_IMAGE_DIR, logger, NUM_CAMERA_VIEWS
 from alice.log import init_log
+from alice.models.labels.crop import CropLabels
+from alice.utils.image import compute_image_hash_similarity
+from alice.clip import CLIP
 
 class Specimen:
     
@@ -14,11 +21,16 @@ class Specimen:
     # e.g. 011250151_additional(1) => image_id=1
     # Brackets around e.g. (1) are optional - will work for Tri434015_additional_4
     re_filename = re.compile(r'additional_?\(?(?P<image_idx>[1-4])\)?$')
+    clip = CLIP()
     
     def __init__(self, specimen_id, paths: List[Path]):
         init_log(specimen_id)
         self.specimen_id = specimen_id
         self.paths = self._get_sorted_paths(paths)
+        try:
+            assert len(self.paths) == NUM_CAMERA_VIEWS
+        except AssertionError as e:        
+            raise(AssertionError(f"{len(self.paths)} images found"))
         
     def _parse_filename_idx(self, path:Path):
         m = self.re_filename.search(path.stem)
@@ -34,26 +46,79 @@ class Specimen:
         
     def get_views(self):
         views = [AngledView(path, i)  for i, path in enumerate(self.paths)]
-        assert len(views) == 4
         return views
             
     def get_label_quartets(self):
         views = self.get_views()
-        modal = mode([len(view.labels) for view in views]) 
-        self._validate_num_labels_per_view(views, modal.mode)          
-        quartets = {}
-        for label_index in range(0, modal.mode):
-            quartet = LabelQuartet()
-            for view in views:
-                label = view.labels.get(label_index, InvalidLabel())
-                quartet.add_label(label)     
-            quartets[label_index] = quartet                          
+        
+        label_levels = self._group_cropped_labels_by_level(views)
+        
+        if not self._label_levels_are_similar_images(label_levels):
+            logger.info('Label images across levels are disimilar - regrouping by similarity')        
+            label_levels = self._regroup_labels_by_similarity(label_levels)
+        else:
+            logger.info('Label similarity check - passed.')  
+            
+        quartets = []
+        for i, labels in label_levels.items():
+            # If we only have one label in the cluster, remove any with max edge < 150
+            # Just a fragment of a label 
+            if len(labels) == 1:
+                max_edge = max(labels[0].shape[:2])
+                if max_edge < 150:
+                    logger.info('Level %s: has 1 small label (max edge %s). Ignoring', i, max_edge)
+            
+            logger.info('Level %s: %s labels', i, len(labels))
+            quartets.append(LabelQuartet(labels))                        
         return quartets
+    
+    def _group_cropped_labels_by_level(self, views):
+        num_levels = max([len(view.labels) for view in views])
+        label_levels = {}
+        for level in range(0, num_levels):
+            # If a label doesn't exist at a particular level for a view, we create InvalidLabel()
+            # So that order of labels will be preserved in the crop & rotate
+            label_levels[level] = CropLabels([view.labels.get(level, InvalidLabel()) for view in views]).crop() 
+        return label_levels 
+    
+    def _label_levels_are_similar_images(self, label_levels):
+        # Permissive similarity - hash comparison for mtaching images ~ 13
+        # But we assume our initial label view arrangement is correct 
+        threshold = 20    
+
+        for labels in label_levels.values():
+        
+            image_list = [Image.fromarray(label) for label in labels]
+            num_images = len(labels)    
+            scores = []
+            for i, j in itertools.combinations(range(num_images), 2):        
+                image1 = image_list[i]
+                image2 = image_list[j]            
+                hash_similarity = compute_image_hash_similarity(image1, image2)
+                scores.append(hash_similarity)
+
+            is_similar = np.all(np.array(scores) < threshold)
+            # If the images in a row aren't similar, we need to cluster the 
+            if not is_similar:
+                return False
+            
+        return True
+    
+    def _regroup_labels_by_similarity(self, label_levels):  
+        all_labels = list(itertools.chain(*label_levels.values()))
+        clusters = self.clip.cluster(all_labels)
+        logger.info('%s clusters with similar images detected', len(clusters))        
+        clustered_images = {}
+        for i, cluster in enumerate(clusters):
+            images = [all_labels[image_idx] for image_idx in cluster]
+            logger.info('Cluster %s has %s matching images', i, len(images))            
+            clustered_images[i] = images
+        return clustered_images
         
     def process(self):
         quartets = self.get_label_quartets()
         all_results = {}
-        for level, quartet in quartets.items(): 
+        for level, quartet in enumerate(quartets): 
             logger.info('Processing quartet level %s', level)
             logger.info('Quartet %s: has %s cropped labels for text detection', level, len(quartet._labels))
             
@@ -76,32 +141,22 @@ class Specimen:
             all_results[level] = results
 
         return all_results
-                
-            
-    @staticmethod
-    def _validate_num_labels_per_view(views, mode):
-        for i, view in enumerate(views):
-            logger.info('View %s: %s labels', i, len(view.labels)) 
-            if len(view.labels) != mode:
-                lower_labels = list(view.labels.keys())[1:]
-                for lower_label in lower_labels:
-                    view.labels[lower_label].set_valid(LabelValid.NONDETECTED_LABELS) 
 
 if __name__ == "__main__":
 
     # logger.set_specimen_id('011250151')
-    # path = PROCESSING_IMAGE_DIR / '011250151_additional(1).JPG'    
+    # path = RESIZED_IMAGE_DIR / '011250151_additional(1).JPG'    
     # view = SpecimenAngledView(path)    
     
     # specimen_id = '011244568'
     
-    # paths = [PROCESSING_IMAGE_DIR / f'011245996_additional_{i}.jpeg' for i in range(1,5)]
+    # paths = [RESIZED_IMAGE_DIR / f'011245996_additional_{i}.jpeg' for i in range(1,5)]
     
     # specimen_id = '011250151'    
-    # paths = [PROCESSING_IMAGE_DIR / f'011250151_additional({i}).jpg' for i in range(1,5)]
+    # paths = [RESIZED_IMAGE_DIR / f'011250151_additional({i}).jpg' for i in range(1,5)]
     # specimen_id = '011250151'   
-    specimen_id = '013638567'    
-
-    paths = [p.resolve() for p in PROCESSING_IMAGE_DIR.glob(f'{specimen_id}*.*') if p.suffix.lower() in {".jpg", ".jpeg"}] 
-    specimen = Specimen(specimen_id, paths)
+    specimen_id = '012509519'
+    specimen_id = 'Tri434015'        
+    paths = [p.resolve() for p in RESIZED_IMAGE_DIR.glob(f'{specimen_id}*.*') if p.suffix.lower() in {".jpg", ".jpeg"}] 
+    specimen = Specimen(specimen_id, paths)    
     labels = specimen.process()
